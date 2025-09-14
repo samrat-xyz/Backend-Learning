3387559051ea65fca1a6c841325da618287d2785
Asif-vai <mr3366533@gmail.com>
2025-09-14 20:45:26 +0600

    Path System

--- NodeJs/Path/index.js
+++ NodeJs/Path/index.js
@@ -2,16 +2,17 @@ import path from 'node:path';
 import {fileURLToPath} from 'url';
 
 const __fileName = fileURLToPath(import.meta.url);
+
 console.log('Full file Name :',__fileName);
 
 const __dirname = path.dirname(__fileName);
-console.log(__dirname)
+console.log("Directory Name",__dirname)
 
 const __basename = path.basename(__fileName);
 
-console.log(__basename)
+console.log("BaseName :",__basename)
 
 
 const __extname = path.extname(__fileName)
 
-console.log(__extname)
+console.log("Extension Name :",__extname)
